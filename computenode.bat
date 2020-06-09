090f8038ec41164aa2e1a8ed6fa8f4303d5bed61
Will Lehman <willyplehman@gmail.com>
2020-06-09 12:17:34 -0500

    add reference to gridgaincompute.jar and absolute path to default-confix.xm.

--- computenode.bat
+++ computenode.bat
@@ -28,13 +28,13 @@ set JVM_OPTS=%JVM_OPTS% -Djavax.xml.parsers.SAXParserFactory=com.sun.org.apache.
 set JVM_OPTS=%JVM_OPTS% -DWriteFilesToDelete=true
 
 rem -verbose:class
-set USER_LIBS=%WAT_HOME%\jar\sys\ignite\*;%WAT_HOME%\jar\*;%WAT_HOME%\jar\rmi\*;%WAT_HOME%\jar\sys\*;%WAT_HOME%\jar\ext\*;%WAT_HOME%\jar\sys\excel\*;%WAT_HOME%\jar\sys\akka\*;%WAT_HOME%\jar\sys\ratingEditor\*;%WAT_HOME%\..\apps\HEC-HMS\*;%WAT_HOME%\jar\sys\geotools\*
+set USER_LIBS=C:\Programs\DC\Plugin\gridgaincompute.jar;%WAT_HOME%\jar\sys\ignite\*;%WAT_HOME%\jar\*;%WAT_HOME%\jar\rmi\*;%WAT_HOME%\jar\sys\*;%WAT_HOME%\jar\ext\*;%WAT_HOME%\jar\sys\excel\*;%WAT_HOME%\jar\sys\akka\*;%WAT_HOME%\jar\sys\ratingEditor\*;%WAT_HOME%\..\apps\HEC-HMS\*;%WAT_HOME%\jar\sys\geotools\*
 rem this isn't a mistake, its supposed to clear the variable so that the path doesn't keep growing
 set IGNITE_LIBS=
 
 :: if WAT_HOME contains spaces,  CONFIG_FILE will contain spaces and gg_computenode_start.bat freaks out.
 :: I think this %%~sA trick is known to have problems in xp before sp1...
-set CONFIG_FILE="%WAT_HOME%\ignite\default-config.xml"
+set CONFIG_FILE="C:\Programs\DC\default-config.xml"
 for %%A in (%CONFIG_FILE%) do set shortconfig=%%~sA
 
 cd "%WAT_HOME%"
